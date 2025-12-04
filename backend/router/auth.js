5ed07a31f0b88ef79485b12083d8eef55d09df08
Zoltan Bagdany <bgzoltan@gmail.com>
2025-12-04 10:53:26 +1000

    be: login email and password check and token are added

--- backend/router/auth.js
+++ backend/router/auth.js
@@ -1,19 +1,43 @@
 import express from "express";
 import { validateAuth } from "../schema/auth.js";
+import bcrypt from "bcrypt";
+import { Users } from "../schema/users.js";
 export const router = express.Router();
 
 router.post("/login", async (req, res) => {
-  const auth = req.body;
+  const credentials = req.body;
 
   try {
-    const { error } = validateAuth(auth);
+    const { error } = validateAuth(credentials);
 
-    if (error || error == undefined) {
-      const err = new Error(error.message);
+    if (error) {
+      const err = new Error(
+        error.message ? error.message : "Invalid login data."
+      );
       err.status = 400;
       throw err;
     } else {
-      res.status(200).send("Login is correct");
+      const { email, password } = auth;
+      const user = await Users.findOne({ email: email });
+
+      if (!user) {
+        let error = new Error("Invalid user data.");
+        error.status = 401;
+        throw error;
+      }
+
+      const isValid = await bcrypt.compare(password, user.password);
+      if (!isValid) {
+        let error = new Error("Invalid user data.");
+        error.status = 401;
+        throw error;
+      }
+
+      // * Generate token for the new user and send it back to the frontend with the user data without the password
+      const token = user.generateToken();
+      const userObject = user.toObject();
+      delete userObject.password;
+      return res.header("x-auth-token", token).status(200).send(userObject);
     }
   } catch (err) {
     console.log("Error: ", err);
